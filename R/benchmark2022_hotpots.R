ccd1fdfb8dd8ccbe75c260e11b65c75a291dd73a
Joshua French <joshua.french@gmail.com>
2023-07-06 20:11:30 -0600

    Update benchmark2020 to benchmark2022.
    Update benchmark2020_hotspots to benchmark2022_hotspots.

--- R/benchmark2022_hotpots.R
+++ R/benchmark2022_hotpots.R
@@ -1,4 +1,4 @@
-#' Hotspots for benchmark2020 data
+#' Hotspots for benchmark2022 data
 #' 
 #' The hotspot region indices for each benchmark
 #' data set 
@@ -16,7 +16,7 @@
 #' 
 #' @name irural02_hotspot
 #' @aliases iclusters_hotspots
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @docType data
 #' @format A matrix
 #' @keywords data
@@ -24,265 +24,265 @@
 NULL
 
 #' @name irural03_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name irural04_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name irural05_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name irural06_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name irural07_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name irural08_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name irural09_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name irural10_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name irural11_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name irural12_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name irural13_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name irural14_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name irural15_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name irural16_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name imixed02_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name imixed03_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name imixed04_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name imixed05_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name imixed06_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name imixed07_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name imixed08_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name imixed09_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name imixed10_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name imixed11_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name imixed12_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name imixed13_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name imixed14_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name imixed15_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name imixed16_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name iurban02_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name iurban03_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name iurban04_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name iurban05_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name iurban06_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name iurban07_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name iurban08_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name iurban09_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name iurban10_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name iurban11_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name iurban12_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name iurban13_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name iurban14_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name iurban15_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
 
 #' @name iurban16_hotspot
-#' @rdname benchmark2020_hotspots
+#' @rdname benchmark2022_hotspots
 #' @format NULL
 #' @docType data
 NULL
